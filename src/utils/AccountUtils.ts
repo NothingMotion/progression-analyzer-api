c28997856eb7a1d7f7068da9e6297cec0f2d70a2
itsma8di <mrmmrahmati@gmail.com>
2025-04-17 23:59:11 +0330

    feat: Add access token validation endpoint and update account methods to return only BrawlStarsAccount

--- src/utils/AccountUtils.ts
+++ src/utils/AccountUtils.ts
@@ -16,7 +16,7 @@ class AccountUtils {
   static convertTag(tag: string): string {
     return "#" + tag.toUpperCase();
   }
-  static async getAccount(tag: string): Promise<BrawlStarsAccount | Error> {
+  static async getAccount(tag: string): Promise<BrawlStarsAccount> {
     try {
       const account = await BrawlStarsAPI.get(tag);
       const extra = await BrawlStarsAPI.getExtra(tag);
@@ -63,7 +63,7 @@ class AccountUtils {
     }
   }
 
-  static async updateAccount(tag: string): Promise<BrawlStarsAccount | Error> {
+  static async updateAccount(tag: string): Promise<BrawlStarsAccount> {
     try {
       const savedAccount = await AccountModel.findOne({
         "account.tag": tag,
