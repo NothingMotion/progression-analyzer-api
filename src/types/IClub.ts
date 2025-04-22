a7bbecfc1fd53b6d7bc9335078d9638b1757a0f2
itsma8di <mrmmrahmati@gmail.com>
2025-04-22 23:37:04 +0330

    feat: add IBattle and IClub type definitions

---
+++ src/types/IClub.ts
@@ -0,0 +1,27 @@
+import { Battle } from "./IBattle";
+
+interface ClubMember {
+  tag: string;
+  name: string;
+  nameColor: string;
+  trophies: number;
+  role: string;
+  icon: {
+    id: number;
+  };
+}
+interface IClub {
+  tag: string;
+  name: string;
+  description: string;
+  type: string;
+  badgeId: string;
+  requiredTrophies: number;
+  trophies: number;
+  members: ClubMember[];
+}
+interface ClubActivityStatistics {
+  lastActive: Record<string, Date | undefined>;
+  commonBattles: Battle[];
+  battlesByMode: Record<string, number>;
+}
